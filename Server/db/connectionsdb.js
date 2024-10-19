801a575bfb5e0e92e3a2445f6269a7b3f145a433
Hamza Younis <hamzayounis106@gmail.com>
2024-10-19 14:11:26 +0500

    Refactor mongoose connection to include connection options

--- Server/db/connectionsdb.js
+++ Server/db/connectionsdb.js
@@ -1,7 +1,11 @@
 import { mongoose } from "mongoose";
+const options = {
+  connectTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
+};
 export const connectDb = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URL);
+    const con = await mongoose.connect(process.env.MONGO_URL, options);
     console.log("Mongo DB connected : " + con.connection.host);
   } catch (error) {
     console.log("Mongo DB connection error : " + error.message);
