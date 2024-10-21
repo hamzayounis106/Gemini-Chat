85cdfb48492fb20ed52b8d5cff998f8d31616c35
Hamza Younis <hamzayounis106@gmail.com>
2024-10-21 06:58:28 +0500

    Refactor titleCreation.js to simplify chat title generation

--- Server/Gemini/titleCreation.js
+++ Server/Gemini/titleCreation.js
@@ -5,19 +5,15 @@ const genAI = new GoogleGenerativeAI(GoogleAPI);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const creatChatTitle = async (prompt) => {
-  let title = "";
-
   let history = [];
-  const chat = model.startChat({ history });
+  const chat = model.startChat([]);
   try {
     const promptMessage = `Generate a short, positive conversation title with exactly 2-3 words, based on the following content: "${prompt}". Avoid using quotation marks or extra words.`;
 
     const result = await chat.sendMessage(promptMessage);
 
     const res = await result.response;
-    title = await res.text();
-    history = [];
-    return title;
+    return (title = await res.text());
   } catch (error) {
     console.log(error.message);
     return null;
